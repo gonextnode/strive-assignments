5cc4cfc53543b197b92284fb7337df110dd42046
Sean Knowles <sean.knowles2020@outlook.com>
2021-02-11 16:50:23 +0000

    refactoring logic and code structure in app.js

--- Module1/day9/app.js
+++ Module1/day9/app.js
@@ -1,28 +1,18 @@
 console.log('app.js connected')
 
+// State
 let drawNumbers = [0]
-let boardSquares = [0]
-
-window.onload = function initaliseBoardSquares() {
-  for (let i = 1; i < 75; i++) {
-    boardSquares.unshift(generateRandomNumbers())
-  }
-  console.log(boardSquares)
-}
-
-const handleDraw = () => {
-  generateRandomNumbers()
-  renderHistoricalDrawNode()
-}
+let boards = []
 
 // Generates random numbers between 1 and 75
-const generateRandomNumbers = (max = 75) => {
+const randomNumbers = (max = 75) => {
+  let numArray = []
   const num = Math.floor(Math.random() * max)
   drawNumbers.unshift(num)
   return num
 }
 
-const renderHistoricalDrawNode = () => {
+const historicalDrawComponent = () => {
   const node = document.createElement('DIV')
   const textNode = document.createTextNode(drawNumbers[0])
   node.appendChild(textNode)
@@ -30,16 +20,21 @@ const renderHistoricalDrawNode = () => {
   console.log(textNode)
 }
 
-const renderBoard = () => {
-  generateBoardShell()
+const boardComponent = () => {
+  const board = document.createElement('div')
+  board.className = 'board'
+  board.setAttribute('id', 'board')
+  document.getElementById('boards').appendChild(board)
 }
 
-const generateBoardShell = () => {
+const renderBoard = () => {
   const { value } = document.querySelector('input')
   for (let i = 0; i < value; i++) {
-    const board = document.createElement('div')
-    board.className = 'board'
-    board.setAttribute('id', 'board')
-    document.getElementById('boards').appendChild(board)
+    boardComponent()
   }
+}
+
+const handleDraw = () => {
+  randomNumbers()
+  historicalDrawComponent()
 }
